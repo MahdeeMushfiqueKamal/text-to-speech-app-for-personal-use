2616c961a2458b551fad6121cf1ad02f1d07560a
mmk <mmkdhrubo@gmail.com>
2023-03-15 21:06:38 +0600

    added gcp-bucket

--- main.py
+++ main.py
@@ -3,21 +3,26 @@ import os
 import traceback
 from flask import Flask, render_template, request, redirect
 from google.cloud import texttospeech
+from google.cloud import storage
 
 # # Set up the authentication credentials
 os.environ[
     "GOOGLE_APPLICATION_CREDENTIALS"
 ] = "mobile-doc-key.json"
 
+# define your bucket name
+bucket_name = 'sunny-bastion-375508.appspot.com'
+
+
 # Set up the Flask app
 app = Flask(__name__)
 global_input_text = ""
-
+global_audio_url = ""
 
 # Define the home route
 @app.route("/")
 def home():
-    return render_template("index.html", global_input_text=global_input_text)
+    return render_template("index.html", global_input_text=global_input_text, global_audio_url=global_audio_url)
 
 
 # Define the text-to-speech route
@@ -28,6 +33,15 @@ def tts():
         client = texttospeech.TextToSpeechClient()
     except: 
         return f"<p> {traceback.print_exc()} </p>"
+    
+    ## Create a Cloud Storage client
+    storage_client = storage.Client()
+
+    # Get the Cloud Storage bucket that you want to use
+    bucket = storage_client.bucket(bucket_name)
+
+    # Create a new Blob and specify the audio file name
+    blob = bucket.blob('output.mp3')
     
     # Get the input text from the form
     input_text = request.form["input_text"]
@@ -52,11 +66,16 @@ def tts():
         input=synthesis_input, voice=voice, audio_config=audio_config
     )
 
-    # Save the response to a file
-    with open("static/output.mp3", "wb") as out:
-        out.write(response.audio_content)
+    # Save the audio content to a file in Cloud Storage
+    blob.upload_from_string(response.audio_content, content_type='audio/mpeg')
+    blob.make_public()
 
-    # Return the file as a response to the request
+
+    # Get the URL of the audio file
+    global global_audio_url
+    global_audio_url = blob.public_url
+
+    # Return the audio URL to the user
     return redirect("/")
 
 
